d8393fd8f0b0941b276cbcca2939059dff3ec302
xiaoping <edwardhjp@gmail.com>
2017-08-24 17:10:29 +0800

    init repo

---
+++ demo/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="app">
+  <vue-component class="app-comp" v-model="count">
+    <span>我是组件</span>
+  </vue-component>
+</div>
+</template>
+
+<script>
+import VueComponent from '@/vue-component'
+
+export default {
+  components: { VueComponent },
+  data () {
+    return {
+      count: 34,
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+.app-comp {
+  margin: 200px;
+}
+</style>
